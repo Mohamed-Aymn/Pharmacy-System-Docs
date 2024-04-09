9ddbcb06a16db74022830d12eaaa05d2add3fc85
Mohamed Aymn <mohamedaymn218@gmail.com>
2024-04-09 15:39:03 +0200

    initial commit

---
+++ docusaurus.config.ts
@@ -0,0 +1,128 @@
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+    title: 'Pharmacy System Docs',
+    tagline: '',
+    favicon: 'img/favicon.ico',
+
+    // Set the production url of your site here
+    url: 'https://your-docusaurus-site.example.com',
+    // Set the /<baseUrl>/ pathname under which your site is served
+    // For GitHub pages deployment, it is often '/<projectName>/'
+    baseUrl: '/',
+
+    // GitHub pages deployment config.
+    // If you aren't using GitHub pages, you don't need these.
+    organizationName: 'Mohamed Aymn', // Usually your GitHub org/user name.
+    projectName: 'pharmacy-system-docs', // Usually your repo name.
+
+    onBrokenLinks: 'throw',
+    onBrokenMarkdownLinks: 'warn',
+
+    // Even if you don't use internationalization, you can use this field to set
+    // useful metadata like html lang. For example, if your site is Chinese, you
+    // may want to replace "en" with "zh-Hans".
+    i18n: {
+        defaultLocale: 'en',
+        locales: ['en'],
+    },
+
+    presets: [
+        [
+            '@docusaurus/preset-classic',
+            {
+                docs: {
+                    routeBasePath: '/', // Serve the docs at the site's root
+                    sidebarPath: require.resolve('./sidebars.js'),
+                    // sidebarPath: './sidebars.ts',
+                },
+                blog: false,
+                theme: {
+                    customCss: './src/css/custom.css',
+                },
+            } satisfies Preset.Options,
+        ],
+    ],
+
+    themeConfig: {
+        navbar: {
+            // title: 'My Site',
+            logo: {
+                alt: 'My Site Logo',
+                src: 'img/logo.svg',
+            },
+            items: [
+                {
+                    type: 'docSidebar',
+                    sidebarId: 'tutorialSidebar',
+                    position: 'left',
+                    label: 'Tutorial',
+                    className: 'main-icon'
+                },
+                {
+                    href: 'https://github.com/Mohamed-Aymn',
+                    label: 'GitHub',
+                    position: 'right',
+                },
+                {
+                    href: 'https://linkedin.com/in/mohamedaymn/',
+                    label: 'LinkedIn',
+                    position: 'right',
+                },
+            ],
+        },
+        footer: {
+            style: 'dark',
+            links: [
+                {
+                    title: 'Docs',
+                    items: [
+                        {
+                            label: 'Tutorial',
+                            to: '/docs/intro',
+                        },
+                    ],
+                },
+                {
+                    title: 'Community',
+                    items: [
+                        {
+                            label: 'Stack Overflow',
+                            href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                        },
+                        {
+                            label: 'Discord',
+                            href: 'https://discordapp.com/invite/docusaurus',
+                        },
+                        {
+                            label: 'Twitter',
+                            href: 'https://twitter.com/docusaurus',
+                        },
+                    ],
+                },
+                {
+                    title: 'More',
+                    items: [
+                        {
+                            label: 'Blog',
+                            to: '/blog',
+                        },
+                        {
+                            label: 'GitHub',
+                            href: 'https://github.com/facebook/docusaurus',
+                        },
+                    ],
+                },
+            ],
+            copyright: `Copyright © ${new Date().getFullYear()} Pharmacy System.`,
+        },
+        prism: {
+            theme: prismThemes.github,
+            darkTheme: prismThemes.dracula,
+        },
+    } satisfies Preset.ThemeConfig,
+};
+
+export default config;
